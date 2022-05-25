5dd088340610ec856ca1916a8f08148a0488faa9
Tamboli <dtamboli@147dda81f32b.ant.amazon.com>
2022-05-25 13:04:27 +0530

    added rectified mpt script v1-2

--- managedPipelineTriggering.sh
+++ managedPipelineTriggering.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 tryCount=1
 declare -a currentStageStates
 pipelineTriggered=0
@@ -8,26 +9,30 @@ until [[ $tryCount -lt 121 ]]; do #Configuring for 120 retries 5 seconds per ret
   currentStates=$(aws codepipeline get-pipeline-state --name s3-to-cfn --query "stageStates[].latestExecution.status" --output text) #Gets current pipeline stage states
   currentStageStates=($currentStates)
   len=${#currentStageStates[@]} #Gets number of stages
-  for (( i=0; i<$len; i++ )); #Loop to check state of each stage.
+  for (( i=0; i<len; i++ )); #Loop to check state of each stage.
   do
     s=${currentStageStates[i]};
-    if [["$s" == InProgress]]; #Check if the stage state is InProgress
+    if [[ "$s" == InProgress ]]; #Check if the stage state is InProgress
       then
         echo "A stage is in progress, waiting";
         break #Break from this for-loop and sleep for 5 seconds before retrying
       else
-        a=`expr $len - 1` #Get count of last stage
+        (( a = len - 1)) #Get count of last stage
         if [ $i -eq $a ]  #Check if this is last stage
           then
             echo "Triggering pipeline"
             #Pipeline Triggering code.
             pipelineTriggered=1
-        continue
+          else
+            continue
+        fi
+    fi
   done
-  if [$pipelineTriggered -eq 1] #Check if pipeline was trigerred in preceding for-loop.
+  if [ $pipelineTriggered -eq 1 ] #Check if pipeline was trigerred in preceding for-loop.
   then
     break #Break from the while/until loop
   else
-    tryCount=`expr $tryCount + 1` #Incrementing retry count
-    sleep (5) #Sleep for 5 seconds before retrying
+    ((tryCount++)) #Incrementing retry count
+    sleep 5 #Sleep for 5 seconds before retrying
+  fi
 done
